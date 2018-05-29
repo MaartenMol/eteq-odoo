9fb9429f160f17b204489be4cca4546a62a97022
MaartenMol96 <MaartenMol96@users.noreply.github.com>
2018-05-29 11:01:14 +0200

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -2,6 +2,8 @@ yum install -y yum-utils device-mapper-persistent-data lvm2 epel-release
 
 yum install -y python-pip
 
+pip install --upgrade pip
+
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 yum install -y docker-ce
